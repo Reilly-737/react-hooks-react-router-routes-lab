0c406f29565c3fa2dbb51fed75a9b444198fd079
+ <+>
2023-09-02 10:50:24 -0600

    finished lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,15 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navBar">
+      <NavLink exact to="/" className="navLink">
+        Home
+      </NavLink>
+      <NavLink to="/movies" className="navLink">Movies</NavLink>
+      <NavLink to="/directors" className="navLink">Directors</NavLink>
+      <NavLink to="/actors" className="navLink">Actors</NavLink>
+  </div>)
 }
 
 export default NavBar;
